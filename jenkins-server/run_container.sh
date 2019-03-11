6f7a98084298d249473d66a0d5742aff1c93d381
Theofanis Deligiannis-Virvos <fanis.devir@gmail.com>
2019-03-11 14:33:07 +0200

    Adding another jenkins server based on  Docker In Practice pdf

---
+++ jenkins-server/run_container.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+docker run\
+	--name jenkins_server_container \
+	-p 8080:8080 \
+	-p 50000:50000 \ #for build slaves
+	-v /var/run/docker.sock:/var/run/docker.sock \
+	-v /tmp:/var/jenkins_home \
+	-d \
+	my_jenkins_server
